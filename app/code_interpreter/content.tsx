aa8cb1a1c2336f791c61003eaae1de98081fc0f1
Dodalpaga <dorian.voydie@gmail.com>
2024-09-14 17:14:07 +0200

    Update processing

--- app/code_interpreter/content.tsx
+++ app/code_interpreter/content.tsx
@@ -1,21 +1,7 @@
 // content.tsx
 
 import * as React from 'react';
-import Container from '@mui/material/Container';
-import { keyframes } from '@mui/system';
 import EditorComponent from './EditorComponent';
-import { Padding } from 'maplibre-gl';
-
-const fadeIn = keyframes`
-  0% {
-    opacity: 0;
-    transform: translateY(20px);
-  }
-  100% {
-    opacity: 1;
-    transform: translateY(0);
-  }
-`;
 
 export default function Content() {
   return (
