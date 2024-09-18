ff33c440e08d2c256a9973f351ab8fb6c0a3743b
Dodalpaga <dorian.voydie@gmail.com>
2024-09-18 23:38:40 +0200

    Implementing projects

--- app/projects/code_interpreter/page.tsx
+++ app/projects/code_interpreter/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 import React from 'react';
 import imagePath from '../../public/assets/mountain.png';
-import NavBar from '../../components/navbar';
-import Footer from '../../components/footer';
+import NavBar from '../../../components/navbar';
+import Footer from '../../../components/footer';
 import Content from './content';
-import Loading from '../../components/loading';
+import Loading from '../../../components/loading';
 
-export default function App1() {
+export default function Template() {
   const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
@@ -21,7 +21,15 @@ export default function App1() {
       <div className="flex flex-col items-center justify-between p-4">
         <NavBar brandName="Dorian Voydie" imageSrcPath={imagePath.src} />
       </div>
-      <div className="flex flex-col items-center justify-between p-4">
+      <div
+        className="flex flex-col items-center justify-between p-4"
+        // This is used when you want ti make the content fit in window height : (no scrolling)
+        style={{
+          height: 'calc(100vh - 146px)',
+          position: 'relative',
+          bottom: '0',
+        }}
+      >
         {isLoading ? <Loading /> : <Content />}
       </div>
       <Footer brandName="Dorian Voydie" />
