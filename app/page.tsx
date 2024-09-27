f97daff523049cbda3899e2b0f73ff19d42c2392
Dodalpaga <dorian.voydie@gmail.com>
2024-09-27 22:48:07 +0200

    [REMOVE] Documentation + fixed errors

--- app/page.tsx
+++ app/page.tsx
@@ -27,19 +27,18 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
         <a
-          href="/documentation"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          href="/profile"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Profile{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about this website features.
+            Learn about me !
           </p>
         </a>
 
@@ -78,7 +77,7 @@ export default function Home() {
 
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm ">
         <p className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          You can check the backend documentation&nbsp;
+          You can check the backend API&nbsp;
           <Link href="/docs">
             <code className="font-mono font-bold">here</code>
           </Link>
