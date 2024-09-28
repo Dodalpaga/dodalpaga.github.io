9307d560deb7ae7a710a23772ffca04e53bc50f5
Dodalpaga <dorian.voydie@gmail.com>
2024-09-28 20:12:17 +0200

    Made my website static compatible

--- app/projects/satellite_segmentation/content.tsx
+++ app/projects/satellite_segmentation/content.tsx
@@ -39,7 +39,7 @@ export default function Content() {
       console.log(imageData); // Log the image data URL to verify it's being generated
 
       // Send a POST request to the FastAPI endpoint with the base64 image
-      fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
+      fetch(`${process.env.NEXT_PUBLIC_API_URL_IMG_SEGMENTATION}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
