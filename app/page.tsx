f4b80ee566134026799bda0e88a41541d840761a
Dodalpaga <dorian.voydie@gmail.com>
2024-10-11 21:00:34 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
       </div>
 
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm ">
-        <p className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="api-banner flex w-full justify-center border-b border-gray-300 from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:from-inherit lg:static lg:w-auto lg:rounded-xl">
           You can check the backend API&nbsp;
           <Link
             href={`${process.env.NEXT_PUBLIC_API_URL_DOCS}`}
