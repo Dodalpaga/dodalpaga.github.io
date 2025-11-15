b89ef1998d511e236f64100b3b6a4dc9d4d57d74
Dodalpaga <dorian.voydie@gmail.com>
2025-11-15 23:51:39 +0100

    Removed services

--- app/page.tsx
+++ app/page.tsx
@@ -74,23 +74,6 @@ export default function Home() {
               Learn about my projects in a one and only place !
             </p>
           </a>
-
-          <a
-            href={'https://fr.fiverr.com/s/VY139mV'}
-            target="blank"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Services{' '}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Check out my services here, feel free to reach out !
-            </p>
-          </a>
         </div>
       </div>
 
